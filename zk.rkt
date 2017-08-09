a9135f5d22bc6e579a9badd81ce5668b06ccec15
zaoqi <zaomir@outlook.com>
2017-08-09 21:19:06 +0800

    disj-v->goal0,dodisj-v->goal0

--- zk.rkt
+++ zk.rkt
@@ -72,9 +72,14 @@
 #| DisjV → Goal0 |#
 (define (disj-v->goal0 d)
   (let ((h (sort (disj-v-h d)
-                 (lambda (x y) (> (sized-s (goal-v x)) (sized-s (goal-v y))))))
+                 (lambda (x y) (> (sized-s (goal0-v x)) (sized-s (goal0-v y))))))
         (t (disj-v-t d)))
-    (goal0 (sized (foldl + 0 (map (λ (x) (sized-s (goal-v x))) h))
+    (goal0 (sized (foldl + 0 (map (λ (x) (sized-s (goal0-v x))) h))
                   (delay/name
                    (λ (s)
                      (mplus ((foldl dodisj-v->goal0 (car h) (cdr h)) s) ((goal1->goal0 t) s))))))))
+
+#| Goal0 → Goal0 → (State → Stream State) |#
+(define ((dodisj-v->goal0 g1 g2) s)
+  (mplus ((force (sized-v (goal0-v g1))) s)
+         ((force (sized-v (goal0-v g2))) s)))
