90e800e0af2ca8a58990608d752bd12470068583
zaoqi <zaomir@outlook.com>
2017-08-22 16:15:50 +0800

    hash-map+filter,state-patch,State

--- state.rkt
+++ state.rkt
@@ -19,5 +19,24 @@
 #| (State → Bool) → (State → Bool) → ID → Any → [Var] → Constraint |#
 (struct constraint (check delete kind parm vars))
 
-#| Vector Goal → Vector Constraint → State |#
+#| Vector Goal → Hash ID (Vector Constraint) → State |#
 (struct state (g c))
+
+#| Vector (Values (Vector Goal Vector) Constraint) → StatePatch |#
+(struct state-patch (g c))
+
+#| Hash a b → (a → b → Maybe c) → Hash a c |#
+(define (hash-map+filter h f)
+  (let ([r (hash)])
+    (let loop ([iter (hash-iterate-first h)])
+      (cond
+        [(not iter) r]
+        [(let-values ([(k v) (hash-iterate-key+value h iter)])
+           (f k v)) => (λ (x) (hash-set! r k x) (loop (hash-iterate-next h iter)))]
+        [else (loop (hash-iterate-next h iter))]))))
+
+#| State → State |#
+(define (clean-constraints s) (error))
+
+#| State → Maybe State |#
+(define (run-constraints s) (error))
