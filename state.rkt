bc81f20eb859a3933a5e328de11df163cee18d2b
zaoqi <zaomir@outlook.com>
2017-08-22 17:40:09 +0800

    clean-constraints

--- state.rkt
+++ state.rkt
@@ -25,36 +25,18 @@
 #| Vector (Values (Vector Goal Vector) Constraint) → StatePatch |#
 (struct state-patch (g c))
 
-#| Hash a b → (a → b → Maybe c) → Hash a c |#
-(define (hash-map+filter h f)
-  (let ([r (hash)])
-    (let loop ([iter (hash-iterate-first h)])
-      (cond
-        [(not iter) r]
-        [(let-values ([(k v) (hash-iterate-key+value h iter)])
-           (f k v)) => (λ (x) (hash-set! r k x) (loop (hash-iterate-next h iter)))]
-        [else (loop (hash-iterate-next h iter))]))))
-
-#| (Pos → a → Bool) → Vector a → Vector a |#
-(define (vector-filter+ f v)
-  (let ([l (vector-length v)])
-  (let loop ([i 0] [r '()])
-    (cond
-      [(<= l i) (list->vector r)]
-      [(let ([x (vector-ref v i)])
-         (f i x)) (loop (+ i 1) (cons x r))]
-      [else (loop (+ i 1) r)]))))
-
-#| Vector a → Pos → Vector a |#
-(define (vector-drop-at v i) (vector-append (vector-take i v) (vector-drop (+ i 1) v)))
+#| [State → Maybe State] |#
+(define cleanc '())
 
 #| State → State |#
 (define (clean-constraints s)
-  (let ([s-c (state-c s)])
-  (state (state-g s)
-         (hash-map+filter s-c
-                          (λ (id constraints)
-                            (vector-filter+ (λ (i c) ((constraint-delete c)
+  (let loop ([cs cleanc] [s s])
+    (if (null? cs)
+        s
+        (let ([ns ((car cleanc) s)])
+          (if ns
+              (loop cleanc ns)
+              (loop (cdr cleanc) s))))))
 
 #| State → Maybe State |#
 (define (run-constraints s) (error))
